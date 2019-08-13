5d988ac13229534d748c15fb88c1a82935c64839
alciancio <alcianciotto@gmail.com>
2019-08-12 22:10:35 -0500

    Buttons and api call

--- public/js/menu.js
+++ public/js/menu.js
@@ -1,43 +1,34 @@
 $(document).ready(() => {
 
-    $("#fruitButton").on("click", function (event) {
-        console.log("animal")
-        let smothii_id = $('#smothii-id').attr('data-smothii-event');
-        console.log('smothii_id', smothii_id);
-    });
+                $("#fruitButton").on("click", function (event) {
+                    $.get('/api/smothii/fruit', (dbFruits) => {
+                        // each fruit recipe gets added to array
+                        let fruits = dbFruits;
+                        for (let i=0; i<fruits.length; i++) {
+                          console.log(fruits)  
+                        }})
+                });
 
-    $("#veggieButton").on("click", function (event) {
-        console.log("animal")
-    });
+                $("#veggieButton").on("click", function (event) {
+                    $.get('/api/smothii/vege', (dbVege) => {
+                        // each vege recipe gets added to array
+                        let veggies = dbVege;
+                        for (let i=0; i<veggies.length; i++) {
+                          console.log(veggies)  
+                        }})
+                });
 
-    $("#userButton").on("click", function (event) {
-        console.log("animal")
-        console.log(event)
-    })
-});
+                $("#userButton").on("click", function (event) {
+                    $.get('/api/smothii/user', (dbUser) => {
+                        // each fruit gets added to array
+                        let userRecipes = dbUser;
+                        for (let i=0; i<userRecipes.length; i++) {
+                          console.log(userRecipes)  
+                        }})
+                })
+        })
 
-// smoothii_category == fruit
-// vege
-
-// function loadSmothiis() {
-//     console.log('adding smoothis');
-//     // loop through seeds and add each one
-//     for (let i = 0; i < smothiiSeeds.length; i++) {
-//         console.log(smothiiSeeds[i]);
-//         let newSmothii = {
-//             smothii_name: smothiiSeeds[i].smothii_name,
-//             smothii_description: smothiiSeeds[i].smothii_description,
-//             smothii_creator: smothiiSeeds[i].smothii_creator,
-//             smothii_image_url: smothiiSeeds[i].smothii_image_url,
-//             smothii_category: smothiiSeeds[i].smothii_category
-//         }
-//         $.post('/api/add/smothii', newSmothii);
-//         console.log('added', i, newSmothii.smothii_name);
-//     }
-// }
-
-
-// function checkStock(){
-// if (stock == 0){
-//     <img src="public/images/outofstock" alt="outOfStock"></img>
-// }
+            // function checkStock(){
+            // if (stock == 0){
+            //     <img src="public/images/outofstock" alt="outOfStock"></img>
+            // }
