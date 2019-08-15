e7eb5682c56d6ba9fb26e0d57671589b565f72a5
alciancio <alcianciotto@gmail.com>
2019-08-15 16:01:58 -0500

    Normal looking smoothie cards

--- public/js/menu.js
+++ public/js/menu.js
@@ -3,17 +3,16 @@ $(document).ready(() => {
         $.get('/api/smothii/fruit', (dbFruits) => {
             var fruits = dbFruits;
             for (var i = 0; i < fruits.length; i++) {
-
-                var smoothieCard = $(`<div class='d-inline-block card menuCard' style = 'width: 20rem' data-id='${dbFruits[i].id}'>`);
-                var smoothieImage = $(`<img class='card-img-top smoothieImg' src='${dbFruits[i].smothii_image_url}'>`);
-                $(".menuCard").append(smoothieImage)
-                console.log(smoothieImage)
+                var smoothieCard = $(`<div class='d-inline-block card menuCard' style = 'width: 20rem;' data-id='${dbFruits[i].id}'>`);
+                var smoothieImage = $(`<img class='card-img-top' src='${dbFruits[i].smothii_image_url}'>`);
                 var smoothieBody = $(`<div class='card-body menuCard-body'>`);
                 var smoothieName = $(`<h5 class='card-title menuCard-title'>${dbFruits[i].smothii_name}</h5>`);
                 var smoothiePrice = $(`<p class='card-text menuCard-text'>${dbFruits[i].smothii_price}</p>`)
-                $(".menuCard-body").append(smoothieName)
-                $(".menuCard-body").append(smoothiePrice)
-                $(".menuCard").append(smoothieBody)
+                $(smoothieCard).append(smoothieImage)
+                $(smoothieBody).append(smoothieName)
+                console.log(smoothieCard)
+                $(smoothieBody).append(smoothiePrice)
+                $(smoothieCard).append(smoothieBody)
                 $("#smoothieRecipes").append(smoothieCard)
 
                 // var smoothieCard = $(`<div class='card smothii-small' id=${dbFruits[i].id}>`)
@@ -24,11 +23,12 @@ $(document).ready(() => {
                 // $(".smoothieCard").append(smoothieImage)
                 // $(".smoothieCard").append(smoothieName)
                 // $(".smoothieCard").append(smoothiePrice)
+                //style= 'width: 200px'
             }
         })
     });
 
-    $(document).on('click', '.smothii-card', openVend);
+    $(document).on('click', '.menuCard', openVend);
 
     function openVend(event) {
         console.log('working');
